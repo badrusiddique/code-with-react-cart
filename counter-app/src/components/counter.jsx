1f0568d6f07dc303d5704c38710f15e1260433d8
Badru Siddique <123924787+badrusiddique@users.noreply.github.com>
2024-06-10 08:28:04 +0000

    use-state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,16 +1,36 @@
 import React, { useState } from "react";
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
+  const [tags, setTags] = useState(["1", "2", "3"]);
 
   const formatCount = () => {
     return count === 0 ? "Zero" : count;
   };
 
+  const styleCount = () => {
+    return count === 0
+      ? "badge text-bg-warning m-1"
+      : "badge text-bg-primary m-1";
+  };
+
+  const handleClick = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+    console.log("button click", count);
+  };
+
   return (
     <div>
-      <span className="badge text-bg-primary m-2">{formatCount()}</span>
-      <button className="btn btn-secondary btn-sm m-2">Click Me</button>
+      <span className={styleCount()}>{formatCount()}</span>
+      <button onClick={handleClick} className="btn btn-secondary btn-sm m-2">
+        Click Me
+      </button>
+      <ul>
+        {tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     </div>
   );
 }
